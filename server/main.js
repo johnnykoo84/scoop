ac28056e12f327cabaa04eb505d1405ad303e7b7
Johnny Koo <johnnykoo84@gmail.com>
2017-07-19 18:20:44 +0900

    showing which port it's listening

--- server/main.js
+++ server/main.js
@@ -9,12 +9,12 @@ const devPort = 3001;
 
 if(process.env.NODE_ENV == 'development') {
     console.log('Server is running on development mode');
-    
+
     const config = require('../webpack.dev.config');
     let compiler = webpack(config);
     let devServer = new WebpackDevServer(compiler, config.devServer);
     devServer.listen(devPort, () => {
-        console.log('webpack-dev-server is listening on port', devPort);
+        console.log('webpack-dev-server is listening on port 3001', devPort);
     });
 }
 app.use('/', express.static(__dirname + '/../public'));
@@ -28,5 +28,5 @@ import posts from './routes/posts';
 app.use('/posts', posts);
 
 const server = app.listen(port, () => {
-    console.log('Express listening on port', port);
+    console.log('Express listening on port at 3000', port);
 });
