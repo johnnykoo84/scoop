ad0996a9459450a9257f743d259c0fa49a82f6c0
Johnny Koo <johnnykoo84@gmail.com>
2017-07-25 02:45:26 +0900

    basic router working for navbar

---
+++ client/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        This is Login page
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ client/components/Pricing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pricing extends Component {
+  render() {
+    return (
+      <div>
+        This is Pricing page
+      </div>
+    );
+  }
+}
+
+export default Pricing;
